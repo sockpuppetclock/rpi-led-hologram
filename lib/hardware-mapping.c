23aef41bcac54df682e20093d3bcc683a9d92e29
sockpuppetclock <42406458+sockpuppetclock@users.noreply.github.com>
2025-05-02 20:16:53 -0500

    oh my god

--- lib/hardware-mapping.c
+++ lib/hardware-mapping.c
@@ -41,12 +41,12 @@ struct HardwareMapping matrix_hardware_mappings[] = {
     .e             = GPIO_BIT(25),  // GPIO_BIT(15),  /* RxD kept free unless 1:64 */
 
     /* Parallel chain 0, RGB for both sub-panels */
-    .p0_r1         = GPIO_BIT(12),  /* masks: SPI0_SCKL  */
-    .p0_g1         = GPIO_BIT(5),  /* Not on RPi1, Rev1; use "regular-pi1" instead */
-    .p0_b1         = GPIO_BIT(6),   /* masks: SPI0_CE1   */
-    .p0_r2         = GPIO_BIT(19),   /* masks: SPI0_CE0   */
-    .p0_g2         = GPIO_BIT(13),   /* masks: SPI0_MISO  */
-    .p0_b2         = GPIO_BIT(20),  /* masks: SPI0_MOSI  */
+    .p0_r1         = GPIO_BIT(11),  /* masks: SPI0_SCKL  */
+    .p0_g1         = GPIO_BIT(27),  /* Not on RPi1, Rev1; use "regular-pi1" instead */
+    .p0_b1         = GPIO_BIT(7),   /* masks: SPI0_CE1   */
+    .p0_r2         = GPIO_BIT(8),   /* masks: SPI0_CE0   */
+    .p0_g2         = GPIO_BIT(9),   /* masks: SPI0_MISO  */
+    .p0_b2         = GPIO_BIT(10),  /* masks: SPI0_MOSI  */
 
     /* All the following are only available with 40 GPIP pins, on A+/B+/Pi2,3 */
     /* Chain 1 */
