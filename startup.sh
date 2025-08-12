a3b03c551b174db03f63950848a4be1a8d8e252c
sockpuppetclock <42406458+sockpuppetclock@users.noreply.github.com>
2025-08-11 22:04:25 -0500

    direct paths in startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd "$(dirname "$0")" || exit 1
-echo -e "    Welcome!\n \n    Display \n   starting... \n \n       IP " > ./welcome.txt
-echo " $(ifconfig wlan0 | awk '/inet / {gsub("", $2); print $2}')" >> ./welcome.txt
-cat "./welcome.txt" | ./examples-api-use/text-example --led-rows=64 --led-cols=64 --led-limit-refresh=1500 --led-pixel-mapper="Rotate:270" -C 255,255,255 -f /home/dietpi/u8g2/tools/font/bdf/4x6.bdf -y 9 &
+# cd "$(dirname "$0")" || exit 1
+echo -e "    Welcome!\n \n    Display \n   starting... \n \n       IP " > /rpi-led-hologram/welcome.txt
+echo " $(ifconfig wlan0 | awk '/inet / {gsub("", $2); print $2}')" >> /rpi-led-hologram/welcome.txt
+cat "/rpi-led-hologram/welcome.txt" | /rpi-led-hologram/examples-api-use/text-example --led-rows=64 --led-cols=64 --led-limit-refresh=1500 --led-pixel-mapper="Rotate:270" -C 255,255,255 -f /home/dietpi/u8g2/tools/font/bdf/4x6.bdf -y 9 &
 pp="$!"
 if [[ "$1" != "q" ]];then
   sleep 25
@@ -11,7 +11,7 @@ else
   sleep 0
 fi
 kill -SIGKILL "$pp"
-./utils/text-scroller --led-rows=64 --led-cols=64 --led-limit-refresh=1500 -C0,255,0 "Starting..." --led-pixel-mapper="Rotate:270" -s 3.2 -f /home/dietpi/u8g2/tools/font/bdf/10x20.bdf -y 23 &
+/rpi-led-hologram/utils/text-scroller --led-rows=64 --led-cols=64 --led-limit-refresh=1500 -C0,255,0 "Starting..." --led-pixel-mapper="Rotate:270" -s 3.2 -f /home/dietpi/u8g2/tools/font/bdf/10x20.bdf -y 23 &
 pp="$!"
 if [[ "$1" != "q" ]];then
   sleep 6
@@ -20,4 +20,4 @@ else
 fi
 kill -SIGKILL "$pp"
 
-./utils/hologram-viewer --led-rows=64 --led-cols=64 --led-pwm-dither-bits=2 --led-slowdown-gpio=2 --led-pwm-bits=3 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper="Rotate:270" --led-limit-refresh=1500 -r 6
+/rpi-led-hologram/utils/hologram-viewer --led-rows=64 --led-cols=64 --led-pwm-dither-bits=2 --led-slowdown-gpio=2 --led-pwm-bits=3 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper="Rotate:270" --led-limit-refresh=1500 -r 6
